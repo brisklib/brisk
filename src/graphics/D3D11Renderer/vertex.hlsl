b01927aedb6b03de151194e68acb235b52f8591c
Brisk <brisk@brisklib.com>
2025-03-31 21:58:59 +0200

    Support reversed subpixel order

--- src/graphics/D3D11Renderer/vertex.hlsl
+++ src/graphics/D3D11Renderer/vertex.hlsl
@@ -35,11 +35,11 @@ float4 norm_rect(float4 rect) {
   return float4(min(rect.xy, rect.zw), max(rect.xy, rect.zw));
 }
 
-struct tint_symbol_8 {
+struct tint_symbol_3 {
   uint vidx : SV_VertexID;
   uint inst : SV_InstanceID;
 };
-struct tint_symbol_9 {
+struct tint_symbol_4 {
   noperspective float4 data0 : TEXCOORD0;
   noperspective float4 data1 : TEXCOORD1;
   noperspective float2 uv : TEXCOORD2;
@@ -49,8 +49,8 @@ struct tint_symbol_9 {
 
 VertexOutput vertexMain_inner(uint vidx, uint inst) {
   VertexOutput output = (VertexOutput)0;
-  float2 tint_symbol_10[4] = {(-0.5f).xx, float2(0.5f, -0.5f), float2(-0.5f, 0.5f), (0.5f).xx};
-  float2 position = tint_symbol_10[vidx];
+  float2 tint_symbol_5[4] = {(-0.5f).xx, float2(0.5f, -0.5f), float2(-0.5f, 0.5f), (0.5f).xx};
+  float2 position = tint_symbol_5[vidx];
   float2 uv_coord = (position + (0.5f).xx);
   float4 outPosition = (0.0f).xxxx;
   bool tint_tmp = (asint(constants[1].x) == 0);
@@ -104,15 +104,15 @@ VertexOutput vertexMain_inner(uint vidx, uint inst) {
     }
   }
   output.canvas_coord = outPosition.xy;
-  float2 tint_symbol_5 = transform2D(outPosition.xy);
-  float2 tint_symbol_6 = to_screen(tint_symbol_5);
-  output.position = float4(tint_symbol_6, outPosition.zw);
+  float2 tint_symbol = transform2D(outPosition.xy);
+  float2 tint_symbol_1 = to_screen(tint_symbol);
+  output.position = float4(tint_symbol_1, outPosition.zw);
   return output;
 }
 
-tint_symbol_9 vertexMain(tint_symbol_8 tint_symbol_7) {
-  VertexOutput inner_result = vertexMain_inner(tint_symbol_7.vidx, tint_symbol_7.inst);
-  tint_symbol_9 wrapper_result = (tint_symbol_9)0;
+tint_symbol_4 vertexMain(tint_symbol_3 tint_symbol_2) {
+  VertexOutput inner_result = vertexMain_inner(tint_symbol_2.vidx, tint_symbol_2.inst);
+  tint_symbol_4 wrapper_result = (tint_symbol_4)0;
   wrapper_result.position = inner_result.position;
   wrapper_result.data0 = inner_result.data0;
   wrapper_result.data1 = inner_result.data1;
